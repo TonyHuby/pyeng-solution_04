29e835ae4d76853e7c0f8574925f8be194668719
leksmi <alexdotspb@gmail.com>
2020-01-04 15:45:27 +0300

    Modify intf.py

--- intf.py
+++ intf.py
@@ -2,15 +2,19 @@
 
 from sys import argv
 
-intf = argv[1]
-vlan = argv[2]
+#intf = argv[1]
+#vlan = argv[2]
+
+intf = input('Enter interface name: ')
+vlan = input('Enter vlan number: ')
 
 access_template = ['switchport mode access',
                    'switchport access vlan {}',
                    'switchport nonegotiate',
                    'spanning-tree portfast',
                    'spanning-tree bpduguard enable']
+print('\n' + '-' * 30)
 print('interface {}'.format(intf))
 print('\n'.join(access_template).format(vlan))
 
-print('\nThe script\'s name is ' + argv[0])
+#print('\nThe script\'s name is ' + argv[0])
