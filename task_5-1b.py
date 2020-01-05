2a1544e378568f868a9d75148054aa46d8fb4e08
leksmi <alexdotspb@gmail.com>
2020-01-05 22:42:52 +0300

    Add task_5-1a/b.py

---
+++ task_5-1b.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+london_co = {
+    'r1': {
+        'location': '21 New Globe Walk',
+        'vendor': 'Cisco',
+        'model': '4451',
+        'ios': '15.4',
+        'ip': '10.255.0.1'
+    },
+    'r2': {
+        'location': '21 New Globe Walk',
+        'vendor': 'Cisco',
+        'model': '4451',
+        'ios': '15.4',
+        'ip': '10.255.0.2'
+    },
+    'sw1': {
+        'location': '21 New Globe Walk',
+        'vendor': 'Cisco',
+        'model': '3850',
+        'ios': '3.6.XE',
+        'ip': '10.255.0.101',
+        'vlans': '10,20,30',
+        'routing': True
+    }
+}
+
+dev_name = input('\nEnter device name: ')
+dev_keys = list(london_co[dev_name].keys())
+dev_option = input(dev_keys)
+
+print(london_co[dev_name][dev_option])
