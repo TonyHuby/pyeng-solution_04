f85bcb228dadf005380dabef31d0f234b85e6d30
leksmi <alexdotspb@gmail.com>
2020-01-12 15:41:57 +0300

    Add generate_access_port_config.py

--- check_password.py
+++ check_password.py
@@ -3,6 +3,7 @@
 username = input('Enter username: ')
 password = input('Enter password: ')
 
+# Проверка длины и сложности пароля
 if len(password) < 8:
     print('The password is too short !')
     pass
