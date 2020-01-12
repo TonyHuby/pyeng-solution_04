8b9185d1d8292f45cea1a0e6f5e9eb927abbe249
leksmi <alexdotspb@gmail.com>
2020-01-12 12:12:46 +0300

    Add check_password.py

---
+++ check_password.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#
+username = input('Enter username: ')
+password = input('Enter password: ')
+
+if len(password) < 8:
+    print('The password is too short !')
+    pass
+elif username in password:
+    print('The password has username inside !')
+    pass
+else:
+    print('Password has installed for user "{}"'.format(username))
+    pass
